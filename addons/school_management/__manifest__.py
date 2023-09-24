1d83e41cf70be66bf1eb5df1a3374758e5babf28
thineisan97 <umgodoodev9@gmail.com>
2023-09-24 23:20:40 +0630

    create school management module

---
+++ addons/school_management/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'SCHOOL MANAGEMENT',
+    'version' : '0.1',
+    'author': "TES",
+    'license' : 'LGPL-3',
+    'description' : """This module contains to automate a range of processes such as student enrollment, course scheduling, and more""",
+
+    'depends' : [
+    ],
+    'data' : [
+        
+    ],    
+}
+
